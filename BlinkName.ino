71f76ec2582267d5cb9d0a80a94591462a27bbca
Benjamin Davey <bldavey@deakin.edu.au>
2021-03-10 16:14:15 +1100

    Abstract out blink functions

--- BlinkName.ino
+++ BlinkName.ino
@@ -38,3 +38,21 @@ void loop() {
     delay(1000);
 }
 
+void longBlink(int led) {
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led, LOW);
+    delay(100);
+}
+
+void shortBlink(int led) {
+    digitalWrite(led, HIGH);
+    delay(50);
+    digitalWrite(led, LOW);
+    delay(100);
+}
+
+void pauseBlink(int led) {
+    digitalWrite(led, LOW);
+    delay(950);
+}
