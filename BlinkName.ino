4aa5160479e5924dc44dc8d2a396ccd5fbd79cb5
Benjamin Davey <bldavey@deakin.edu.au>
2021-03-10 16:15:08 +1100

    Abstract out conversion to morse code

--- BlinkName.ino
+++ BlinkName.ino
@@ -38,6 +38,150 @@ void loop() {
     delay(1000);
 }
 
+void blinkString(String name) {
+    for (int i = 0; i < name.length(); i++) {
+        switch(name[i]) {
+            case 'A':
+                shortBlink(led);
+                longBlink(led);
+                break;
+            case 'B':
+                longBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                break;
+            case 'C':
+                longBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                break;
+            case 'D':
+                longBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                break;
+            case 'E':
+                shortBlink(led);
+                break;
+            case 'F':
+                shortBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                break;
+            case 'G':
+                longBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                break;
+            case 'H':
+                shortBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                break;
+            case 'I':
+                shortBlink(led);
+                shortBlink(led);
+                break;
+            case 'J':
+                shortBlink(led);
+                longBlink(led);
+                longBlink(led);
+                longBlink(led);
+                break;
+            case 'K':
+                longBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                break;
+            case 'L':
+                shortBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                break;
+            case 'M':
+                longBlink(led);
+                longBlink(led);
+                break;
+            case 'N':
+                longBlink(led);
+                shortBlink(led);
+                break;
+            case 'O':
+                longBlink(led);
+                longBlink(led);
+                longBlink(led);
+                break;
+            case 'P':
+                shortBlink(led);
+                longBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                break;
+            case 'Q':
+                longBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                break;
+            case 'R':
+                shortBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                break;
+            case 'S':
+                shortBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                break;
+            case 'T':
+                longBlink(led);
+                break;
+            case 'U':
+                shortBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                break;
+            case 'V':
+                shortBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                break;
+            case 'W':
+                shortBlink(led);
+                longBlink(led);
+                longBlink(led);
+                break;
+            case 'X':
+                longBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                break;
+            case 'Y':
+                longBlink(led);
+                shortBlink(led);
+                longBlink(led);
+                longBlink(led);
+                break;
+            case 'Z':
+                longBlink(led);
+                longBlink(led);
+                shortBlink(led);
+                shortBlink(led);
+                break;
+            default:
+                break;
+        }
+        pauseBlink(led);
+    }
+}
+
 void longBlink(int led) {
     digitalWrite(led, HIGH);
     delay(200);
